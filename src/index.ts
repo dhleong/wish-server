911c68550bb1a26836996c6a4d2ac7b1eb3f135d
Daniel Leong <me@dhleong.net>
2018-10-14 19:11:46 -0400

    Add CORS support, Now config, etc.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
 
+import cors from "@koa/cors";
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import Router from "koa-router";
+import serve from "koa-static";
 
 import { logger } from "./log";
 import { createRoutes } from "./routes";
 import { init as initPush } from "./services/push";
 
 const {
+    CORS_HOST,
     PORT,
 } = process.env;
 
 const app = new Koa();
 const router = new Router();
 
+const corsMiddleware = cors({
+    origin: CORS_HOST,
+});
+
 function initServer() {
     app.use(bodyParser());
 
     router.use("/v1", createRoutes().routes());
 
+    app.use(corsMiddleware);
+    app.use(serve("static"));
     app.use(router.routes());
 
     const server = app.listen(PORT, () => {
