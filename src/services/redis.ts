7434d643aa0177307759076978593231c3bd34af
Daniel Leong <me@dhleong.net>
2018-10-20 22:33:19 -0400

    Use redis PING command to test connection

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,6 +1,7 @@
 import { createHandyClient } from "handy-redis";
 import * as redis from "redis";
 import { requireKey } from "../errors";
+import { logger } from "../log";
 
 export const client = createClient();
 
@@ -8,14 +9,20 @@ function createClient() {
     const endpoint: string = requireKey(process.env, "REDIS_ENDPOINT");
     const password = requireKey(process.env, "REDIS_PASSWORD");
 
-    return createHandyClient(`//${endpoint}`, {
+    const handy = createHandyClient(`//${endpoint}`, {
         password,
     });
+
+    handy.redis.on("error", e => {
+        logger.warn("Redis error", {error: e});
+    });
+
+    return handy;
 }
 
 export async function init() {
     // verify connection
-    const pong = await GET("ping");
+    const pong = await client.ping("pong");
     if (pong !== "pong") {
         throw new Error("Unable to verify Redis connection");
     }
